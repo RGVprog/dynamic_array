3351681c6d3bf382ac74d35fd2a819a8b4d55dbb
RGVprog <166976815+RGVprog@users.noreply.github.com>
2024-05-12 19:58:28 +0530

    Optimised sub-optimal code

--- d_array.h
+++ d_array.h
@@ -9,25 +9,29 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-#ifdef D_ARRAY_IMPLEMENTATION
+typedef struct { void *ptr; size_t len; }d_array;
+d_array d_array_create (size_t, size_t);
+void d_array_free(d_array*);
 
-void* d_array_init(size_t size);
-void d_array_reallocate(void* ptr, size_t size)
-
-
-void* d_array_init(size_t size){
-    void *ptr;
-    ptr = malloc(size);
-    return ptr;
-}
-
-void* d_array_reallocate(void* ptr, size_t size){
-    ptr = realloc(ptr, size);
-}
-
-#define d_array(T, len) ((T*)d_array_init(T * len))
-#define d_array_resize(T, ptr, size) ((T*)d_array_reallocate(ptr, size))
+#define d_array_insert(T, index, value, ptr) (((T*)ptr)[index] = value)
+#define d_array_at(T, index, ptr) (((T*)ptr)[index])
 
 #endif
+
+#ifdef D_ARRAY_IMPLEMENTATION
+
+d_array d_array_create (size_t sizeoftype, size_t len){
+    d_array a = {
+        .ptr = malloc(sizeoftype * len),
+        .len = len,
+    };
+
+    return a;
+}
+
+void d_array_free(d_array* a){
+    free(a->ptr);
+    a->ptr = NULL;
+}
 
 #endif
